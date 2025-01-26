f0f8707218a01bcdddf1549d2fcb6c5ce903deae
Phatdat01 <phatdatlht2001@gmail.com>
2025-01-26 10:14:20 +0700

    feat: done navbar main

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("header.html")
         .then(response => response.text())
         .then(data => document.getElementById("header-placeholder").innerHTML = data);
+    // Load main
+    fetch("main.html")
+        .then(response => response.text())
+        .then(data => document.getElementById("main-content").innerHTML = data);
     // Load footer
     fetch("footer.html")
         .then(response => response.text())
